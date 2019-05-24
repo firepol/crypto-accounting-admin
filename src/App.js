f0db06c40fee525df961673f7260455d56a09e50
firepol <1702718+firepol@users.noreply.github.com>
2019-05-24 16:37:35 +0200

    Add accounts edit & create

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Admin, Resource, ListGuesser} from 'react-admin';
+import {Admin, Resource} from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-import { AccountList } from './accounts';
-import { TransactionList } from './transactions';
-import { TradeList } from './trades';
+import {AccountCreate, AccountEdit, AccountList} from './accounts';
+import {TransactionList} from './transactions';
+import {TradeList} from './trades';
 
 const dataProvider = jsonServerProvider('http://127.0.0.1:5000/api');
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="accounts" list={AccountList}/>
+    <Resource name="accounts" list={AccountList} edit={AccountEdit} create={AccountCreate}/>
     <Resource name="trades" list={TradeList}/>
     <Resource name="transactions" list={TransactionList}/>
   </Admin>
--- src/accounts.js
+++ src/accounts.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import { List, Datagrid, TextField } from 'react-admin';
+import {
+  List,
+  Datagrid,
+  TextField,
+  EditButton,
+  Edit,
+  Create,
+  SimpleForm,
+  TextInput,
+  LongTextInput,
+  DisabledInput
+} from 'react-admin';
 
 export const AccountList = props => (
   <List {...props}>
     <Datagrid rowClick="edit">
-      <TextField source="id" />
-      <TextField source="type" />
-      <TextField source="name" />
-      <TextField source="description" />
+      <TextField source="id"/>
+      <TextField source="type"/>
+      <TextField source="name"/>
+      <TextField source="description"/>
+      <EditButton/>
     </Datagrid>
   </List>
+);
+
+export const AccountEdit = props => (
+  <Edit {...props}>
+    <SimpleForm>
+      <DisabledInput source="id"/>
+      <TextInput source="name"/>
+      <TextInput source="type"/>
+      <LongTextInput source="description"/>
+    </SimpleForm>
+  </Edit>
+);
+
+export const AccountCreate = props => (
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput source="name"/>
+      <TextInput source="type"/>
+      <LongTextInput source="description"/>
+    </SimpleForm>
+  </Create>
 );
