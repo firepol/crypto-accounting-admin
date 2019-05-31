bffe378f5f09de4e52296e82c65e1934c1d957cc
firepol <1702718+firepol@users.noreply.github.com>
2019-06-01 00:11:57 +0200

    Remove unneeded code from customJsonServer.js

--- src/customJsonServer.js
+++ src/customJsonServer.js
@@ -1,5 +1,4 @@
-import { stringify } from 'query-string';
-import { fetchUtils, GET_LIST, GET_ONE, GET_MANY, GET_MANY_REFERENCE, CREATE, UPDATE, UPDATE_MANY, DELETE, DELETE_MANY, } from 'react-admin';
+import { fetchUtils, CREATE } from 'react-admin';
 var __assign = (this && this.__assign) || function () {
   __assign = Object.assign || function(t) {
     for (var s, i = 1, n = arguments.length; i < n; i++) {
@@ -36,28 +35,6 @@ export default (function (apiUrl, httpClient) {
     var url = '';
     var options = {};
     switch (type) {
-      case GET_LIST: {
-        var _b = params.pagination, page = _b.page, perPage = _b.perPage;
-        var _c = params.sort, field = _c.field, order = _c.order;
-        var query = __assign({}, fetchUtils.flattenObject(params.filter), { _sort: field, _order: order, _start: (page - 1) * perPage, _end: page * perPage });
-        url = apiUrl + "/" + resource + "?" + stringify(query);
-        break;
-      }
-      case GET_ONE:
-        url = apiUrl + "/" + resource + "/" + params.id;
-        break;
-      case GET_MANY_REFERENCE: {
-        var _d = params.pagination, page = _d.page, perPage = _d.perPage;
-        var _e = params.sort, field = _e.field, order = _e.order;
-        var query = __assign({}, fetchUtils.flattenObject(params.filter), (_a = {}, _a[params.target] = params.id, _a._sort = field, _a._order = order, _a._start = (page - 1) * perPage, _a._end = page * perPage, _a));
-        url = apiUrl + "/" + resource + "?" + stringify(query);
-        break;
-      }
-      case UPDATE:
-        url = apiUrl + "/" + resource + "/" + params.id;
-        options.method = 'PUT';
-        options.body = JSON.stringify(params.data);
-        break;
       case CREATE:
         if (type === 'trades/associate') {
           const { data: { selectedIds } } = params;
@@ -70,17 +47,6 @@ export default (function (apiUrl, httpClient) {
           options.body = JSON.stringify(params.data);
         }
         break;
-      case DELETE:
-        url = apiUrl + "/" + resource + "/" + params.id;
-        options.method = 'DELETE';
-        break;
-      case GET_MANY: {
-        var query = {
-          id: params.ids,
-        };
-        url = apiUrl + "/" + resource + "?" + stringify(query);
-        break;
-      }
       default:
         throw new Error("Unsupported fetch action type " + type);
     }
@@ -96,18 +62,6 @@ export default (function (apiUrl, httpClient) {
   var convertHTTPResponse = function (response, type, resource, params) {
     var headers = response.headers, json = response.json;
     switch (type) {
-      case GET_LIST:
-      case GET_MANY_REFERENCE:
-        if (!headers.has('x-total-count')) {
-          throw new Error('The X-Total-Count header is missing in the HTTP Response. The jsonServer Data Provider expects responses for lists of resources to contain this header with the total number of results to build the pagination. If you are using CORS, did you declare X-Total-Count in the Access-Control-Expose-Headers header?');
-        }
-        return {
-          data: json,
-          total: parseInt(headers
-            .get('x-total-count')
-            .split('/')
-            .pop(), 10),
-        };
       case CREATE:
         return { data: __assign({}, params.data, { id: json.id }) };
       default:
@@ -122,26 +76,6 @@ export default (function (apiUrl, httpClient) {
    */
   return function (type, resource, params) {
     // json-server doesn't handle filters on UPDATE route, so we fallback to calling UPDATE n times instead
-    if (type === UPDATE_MANY) {
-      return Promise.all(params.ids.map(function (id) {
-        return httpClient(apiUrl + "/" + resource + "/" + id, {
-          method: 'PUT',
-          body: JSON.stringify(params.data),
-        });
-      })).then(function (responses) { return ({
-        data: responses.map(function (response) { return response.json; }),
-      }); });
-    }
-    // json-server doesn't handle filters on DELETE route, so we fallback to calling DELETE n times instead
-    if (type === DELETE_MANY) {
-      return Promise.all(params.ids.map(function (id) {
-        return httpClient(apiUrl + "/" + resource + "/" + id, {
-          method: 'DELETE',
-        });
-      })).then(function (responses) { return ({
-        data: responses.map(function (response) { return response.json; }),
-      }); });
-    }
     var _a = convertDataRequestToHTTP(type, resource, params), url = _a.url, options = _a.options;
     return httpClient(url, options).then(function (response) {
       return convertHTTPResponse(response, type, resource, params);
